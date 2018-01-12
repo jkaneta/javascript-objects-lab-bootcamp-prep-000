79b926b4b873ed4956f26614f1eadd907534bc55
J <flatironschool@2joyce.com>
2018-01-12 23:15:14 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -10,3 +10,8 @@ function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
   return object;
 }
 
+function deleteFromObjectByKey(object, key) {
+  var newObj = Object.assign({},object);
+  delete newObj[key];
+  return newObj;
+}
